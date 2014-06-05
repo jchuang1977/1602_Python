2cf5f045a17adde77a66a223661151c47ca778a7
Andy Huang <jchuang1977@gmail.com>
2014-06-05 23:03:35 +0800

    modify lcd_time.py with ip addr

--- lcd_time.py
+++ lcd_time.py
@@ -5,9 +5,19 @@ from time import *
 from datetime import datetime
 
 lcd = lcddriver.lcd()
+cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+
+def run_cmd(cmd):
+       p = Popen(cmd, shell=True, stdout=PIPE)
+       output = p.communicate()[0]
+       return output
+
+
+ipaddr = run_cmd(cmd)
+ipaddrstr = 'IP:' + ipaddr 	
 
 while True:
 	sleep(1)
 	lcd.lcd_clear
 	lcd.lcd_display_string(datetime.now().strftime('%m%d/%y %X'),1)
-	lcd.lcd_display_string(datetime.now().strftime('%a %b %d %Y'),2)
+	lcd.lcd_display_string(ipaddrstr,2)
