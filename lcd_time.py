d0e956b5b7746e38b954601e20ccf7d574daa065
Andy Huang <jchuang1977@gmail.com>
2014-06-05 22:47:10 +0800

    modify lcd_time.py

--- lcd_time.py
+++ lcd_time.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 lcd = lcddriver.lcd()
 
-while True;
+while True:
 	sleep(1)
-	lcd.clear
-	lcd.lcd_display_string("  Hello world  ", 1)
-	lcd.lcd_display_string(datetime.now().strftime('  %I : %M : %S \n%a %b %d %Y'),2)
+	lcd.lcd_clear
+	lcd.lcd_display_string(datetime.now().strftime('  %I : %M : %S'),1)
+	lcd.lcd_display_string(datetime.now().strftime('  %a %b %d %Y'),2)
